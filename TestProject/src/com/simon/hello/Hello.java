44b7c672688aef44a35b96665a0c6a388f5bcf94
simon <simon@192.168.120.129>
2014-10-19 20:40:22 +0200

    commit4

--- TestProject/src/com/simon/hello/Hello.java
+++ TestProject/src/com/simon/hello/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Test commit");
+		System.out.println("Test 4 commit");
 	}
 
 }
