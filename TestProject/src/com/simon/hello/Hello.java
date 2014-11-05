709d9caafc51c325e922a3991a5562ac1886707b
simon <simon_t@poczta.fm>
2014-11-05 22:58:04 +0100

    Test commit

--- TestProject/src/com/simon/hello/Hello.java
+++ TestProject/src/com/simon/hello/Hello.java
@@ -6,6 +6,7 @@ public class Hello {
 		// TODO Auto-generated method stub
 		System.out.println("Test commit");
 		System.out.println("Test 4 commit");
+		System.out.println("Test 4 commit");
 	}
 
 }
